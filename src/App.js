6769257abebb58d92a8733a591f77c2416b1b397
Alan Moran <nebsy@allstate.com>
2017-11-26 16:24:45 -0600

    Shows contract owner on aptp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import AnnouncementBox from '../build/contracts/AnnouncementBox.json'
 import getWeb3 from './utils/getWeb3'
 
 import './css/oswald.css'
@@ -12,7 +12,8 @@ class App extends Component {
     super(props)
 
     this.state = {
-      storageValue: 0,
+      secretAnnouncements: [],
+      announcementBoxInstance: null,
       web3: null
     }
   }
@@ -26,8 +27,6 @@ class App extends Component {
       this.setState({
         web3: results.web3
       })
-
-      // Instantiate contract once web3 provided.
       this.instantiateContract()
     })
     .catch(() => {
@@ -36,34 +35,21 @@ class App extends Component {
   }
 
   instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
-
     const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+    const announcementBox = contract(AnnouncementBox)
+    announcementBox.setProvider(this.state.web3.currentProvider)
 
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+    console.log('banana 1 - a')
 
-    // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(10, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
+    announcementBox.deployed().then((instance) => {
+      console.log('banana 3')
+      this.setState({announcementBoxInstance: instance})
+      return instance
+    }).then((result) => {
+      console.log('banana 4')
+      return this.state.announcementBoxInstance.owner.call()
+    }).then((result) => {
+      return this.setState({ owner: result })
     })
   }
 
@@ -77,12 +63,7 @@ class App extends Component {
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+              <p>This contract was deployed by: {this.state.owner}</p>
             </div>
           </div>
         </main>
